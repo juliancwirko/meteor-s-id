8fcf01fb104418c6ee89df73405f76643015bfd9
Julian Ćwirko <julian.cwirko@gmail.com>
2016-04-10 18:51:19 +0200

    styles update

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
     summary: 'Simple user accounts for Meteor.',
-    version: '3.1.3',
+    version: '3.2.0',
     name: 'juliancwirko:s-id',
     git: 'https://github.com/juliancwirko/meteor-s-id.git'
 });
